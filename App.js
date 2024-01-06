a5d946de3d428dc053623f3e336e73d5807eef09
Jibbs Dev <68401777+JibbsDev@users.noreply.github.com>
2024-01-06 03:08:35 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RouletteBetting from './RouletteBetting';
+
+function App() {
+  return (
+    <div className="App">
+      <RouletteBetting />
+    </div>
+  );
+}
+
+export default App;
